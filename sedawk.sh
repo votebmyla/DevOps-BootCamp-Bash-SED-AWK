ad91965b5af6d839ee6e6c915f551be293cbe1e5
votebmyla <votebmyla@gmail.com>
2022-12-27 15:13:53 +0500

    add change uid

--- sedawk.sh
+++ sedawk.sh
@@ -6,3 +6,4 @@ awk -i inplace '/saned/ {sub(/usr\/sbin\/nologin/, "bin/bash")} 1' passwd_new
 sed -i '/avahi/ s/usr\/sbin\/nologin/bin\/bash/' passwd_new
 awk -i inplace -F: -v OFS=: '{print $1,$3,$5,$7}' passwd_new
 sed -i '/daemon/d' passwd_new
+awk -i inplace -F: -v OFS=: '($2%2)==0 {$NF="/bin/zsh"} 1' passwd_new
