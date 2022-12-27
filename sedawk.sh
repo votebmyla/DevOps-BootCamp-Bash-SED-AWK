40b386e8dc16053054ea1a3f127b84bc4810b4f1
votebmyla <votebmyla@gmail.com>
2022-12-27 14:22:51 +0500

    add save only columns 1,3,5 and 7

--- sedawk.sh
+++ sedawk.sh
@@ -4,3 +4,4 @@
 cp passwd ./passwd_new
 awk -i inplace '/saned/ {sub(/usr\/sbin\/nologin/, "bin/bash")} 1' passwd_new
 sed -i '/avahi/ s/usr\/sbin\/nologin/bin\/bash/' passwd_new
+awk -i inplace -F: '{print $1,$3,$5,$7}' passwd_new
