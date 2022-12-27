c0a5abf6f3faa671edf8e148c1a101252f252578
votebmyla <votebmyla@gmail.com>
2022-12-27 14:21:37 +0500

    add change shell for avahi user

--- sedawk.sh
+++ sedawk.sh
@@ -3,3 +3,4 @@
 # Put you code here
 cp passwd ./passwd_new
 awk -i inplace '/saned/ {sub(/usr\/sbin\/nologin/, "bin/bash")} 1' passwd_new
+sed -i '/avahi/ s/usr\/sbin\/nologin/bin\/bash/' passwd_new
