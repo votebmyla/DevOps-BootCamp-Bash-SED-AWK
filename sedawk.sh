f8c4d2e781bdedf0b83ba23805d79d44ff0985cd
votebmyla <votebmyla@gmail.com>
2022-12-27 14:32:58 +0500

    fix output field separator

--- sedawk.sh
+++ sedawk.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Put you code here
-cp passwd ./passwd_new
+cat passwd > passwd_new
 awk -i inplace '/saned/ {sub(/usr\/sbin\/nologin/, "bin/bash")} 1' passwd_new
 sed -i '/avahi/ s/usr\/sbin\/nologin/bin\/bash/' passwd_new
-awk -i inplace -F: '{print $1,$3,$5,$7}' passwd_new
+awk -i inplace -F: -v OFS=: '{print $1,$3,$5,$7}' passwd_new
 sed -i '/daemon/d' passwd_new
